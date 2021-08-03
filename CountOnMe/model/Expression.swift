e5327a4d13a266118c654352dfda32d2ee7405c3
Gilles David <gdvd@me.com>
2021-08-03 18:11:26 +0200

    Modifications

--- CountOnMe/model/Expression.swift
+++ CountOnMe/model/Expression.swift
@@ -16,16 +16,12 @@ class Expression {
     
     //MARK: - Control
     private var canAddOperator: Bool {
-        let ele = lastElement(exp)
+        let ele = elements(exp)
         return ele.last != "+" 
             && ele.last != "-" 
             && ele.last != "*" 
             && ele.last != "/"
-            && exp.count != 0
-    }
-    
-    private func lastElement(_ ele: String) -> [String] {
-        return ele.split(separator: " ").map { "\($0)" }
+            && ele.count != 0
     }
     
     private var expressionHaveResult: Bool {
@@ -74,7 +70,7 @@ class Expression {
     }
     
     public func addPoint() -> express {
-        if let lastelement = lastElement(exp).last {
+        if let lastelement = elements(exp).last {
             if canAddOperator {
                 if expressionHavePoint(lastelement){
                     return express.failure(.errorOnlyOnePoint)
@@ -110,9 +106,6 @@ class Expression {
         } else {
             return express.failure(.errorExpression)
         }
-        if expressionHaveResult {
-            exp = ""
-        }
         return express.success(exp)
     }
 
@@ -136,7 +129,7 @@ class Expression {
                 }
     }
     
-    //MARK: Addition & Soustraction
+    //MARK: - Addition & Soustraction
     private func additionAndSoustraction(_ opeToReduce: [String]) -> express {
         var operationsToReduce = opeToReduce
         // Iterate over operations while an operand still here
@@ -158,7 +151,7 @@ class Expression {
         return express.success(operationsToReduce.joined(separator: " "))
     }
      
-    //MARK: Multiplication & Division
+    //MARK: - Multiplication & Division
     private func multiplicationAndDivision(_ opeToReduce: [String]) -> express {
         var operationsToReduce = opeToReduce
         var position = 1
@@ -181,13 +174,13 @@ class Expression {
                     }
                 default: break
                 }
-                
+                // Suppress 3 args
                 let indexToDelete = [position - 1, position, position + 1]
                 operationsToReduce = operationsToReduce
                     .enumerated()
                     .filter { !indexToDelete.contains($0.offset) }
                     .map { $0.element }
-                
+                // Insert result
                 operationsToReduce.insert("\(String(format:"%.2f", result))", at: position - 1)
                 
             } else {
